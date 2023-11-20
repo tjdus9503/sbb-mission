d88c242e747255aafde29407c0e62838353d3d3e
tjdus9503 <tjdus03095@gmail.com>
2023-11-20 15:11:32 +0900

    init

---
+++ src/main/java/com/ll/sbbmission/SbbMissionApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
