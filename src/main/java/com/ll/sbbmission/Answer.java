2a3b1bf046a6758f9e571666ca9b7ab9c9093ecf
tjdus9503 <tjdus03095@gmail.com>
2023-11-22 17:07:23 +0900

    2-04 Entity-Question,Answer

---
+++ src/main/java/com/ll/sbbmission/Answer.java
@@ -0,0 +1,24 @@
+package com.ll.sbbmission;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/ll/sbbmission/Question.java
@@ -0,0 +1,36 @@
+package com.ll.sbbmission;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+    @Builder
+    public Question(String subject, String content, LocalDateTime createDate) {
+        this.subject = subject;
+        this.content = content;
+        this.createDate = createDate;
+    }
+}
