ce8cb0efe4e65c4aeaf92645436ce6725e9a2ed9
tjdus9503 <tjdus03095@gmail.com>
2023-11-24 07:06:49 +0900

    2-10, feat: /question/detail/{id}

--- src/main/java/com/ll/sbbmission/domain/question/QuestionService.java
+++ src/main/java/com/ll/sbbmission/domain/question/QuestionService.java
@@ -1,16 +1,31 @@
 package com.ll.sbbmission.domain.question;
 
+import com.ll.sbbmission.global.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
+
     private final QuestionRepository questionRepository;
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(int id) {
+
+        Optional<Question> optQuestion = questionRepository.findById(id);
+
+        if (optQuestion.isPresent()) {
+            return optQuestion.get();
+        }
+        else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
