b955813b64ca04d568294b1f30d9e85a746f0045
tjdus9503 <tjdus03095@gmail.com>
2023-11-30 06:55:29 +0900

    3-02, test: make test data

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,12 +1,14 @@
 package com.ll.sbbmission;
 
 import com.ll.sbbmission.domain.answer.entity.Answer;
-import com.ll.sbbmission.domain.answer.repository.AnswerRepository;
 import com.ll.sbbmission.domain.question.entity.Question;
 import com.ll.sbbmission.domain.question.repository.QuestionRepository;
+import com.ll.sbbmission.domain.question.service.QuestionService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,16 +18,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Transactional
 class SbbMissionApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
 
     @Autowired
-    private AnswerRepository answerRepository;
+    private QuestionService questionService;
 
-    @Transactional
     @Test
+    @DisplayName("테스트")
     void testJpa() {
         Optional<Question> oq = this.questionRepository.findById(2);
         assertTrue(oq.isPresent());
@@ -35,5 +38,16 @@ class SbbMissionApplicationTests {
 
         assertEquals(1, answerList.size());
         assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
+
+    @Test
+    @DisplayName("테스트 데이터")
+    @Rollback(false)
+    void testData() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.createQuestion(subject, content);
+        }
     }
 }
