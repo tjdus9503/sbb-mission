12ec5c9154d812b33c03212f618298f635399400
tjdus9503 <tjdus03095@gmail.com>
2023-11-30 14:42:49 +0900

    3-08, feat: PreAuthorize

--- src/main/java/com/ll/sbbmission/domain/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbbmission/domain/question/controller/QuestionController.java
@@ -8,6 +8,7 @@ import com.ll.sbbmission.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,11 +41,13 @@ public class QuestionController {
         return "question/question_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String create(QuestionForm questionForm) {
         return "question/question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     public String create(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
         if (bindingResult.hasErrors()) {
