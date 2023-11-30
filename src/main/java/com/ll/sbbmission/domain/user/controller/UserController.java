750ff3724fbd64a75d76904250d0b578a5001c0e
tjdus9503 <tjdus03095@gmail.com>
2023-11-30 11:26:59 +0900

    3-06, chore: UserCreateForm, UserController

---
+++ src/main/java/com/ll/sbbmission/domain/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.ll.sbbmission.domain.user.controller;
+
+import com.ll.sbbmission.domain.user.UserCreateForm;
+import com.ll.sbbmission.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "/user/signup_form";
+    }
+
+    @PostMapping("signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+
+        return "redirect:/";
+    }
+}
