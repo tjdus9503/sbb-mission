a03fa99b3c8088f7cf4ff4d2649fd99e2f365e92
tjdus9503 <tjdus03095@gmail.com>
2023-11-30 13:54:22 +0900

    3-07, feat: login

--- src/main/java/com/ll/sbbmission/domain/user/repository/UserRepository.java
+++ src/main/java/com/ll/sbbmission/domain/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.ll.sbbmission.domain.user.repository;
 import com.ll.sbbmission.domain.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
--- src/main/java/com/ll/sbbmission/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sbbmission/global/security/SecurityConfig.java
@@ -1,7 +1,9 @@
-package com.ll.sbbmission.global;
+package com.ll.sbbmission.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,5 +35,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
