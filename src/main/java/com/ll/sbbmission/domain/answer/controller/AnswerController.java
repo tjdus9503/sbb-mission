f0daa3230c3f16f959c99d13abca40b91a91a67c
tjdus9503 <tjdus03095@gmail.com>
2023-12-01 20:35:21 +0900

    3-11, feat: 답변 추천

--- src/main/java/com/ll/sbbmission/domain/answer/controller/AnswerController.java
+++ src/main/java/com/ll/sbbmission/domain/answer/controller/AnswerController.java
@@ -91,4 +91,15 @@ public class AnswerController {
 
         return "redirect:/question/detail/%s".formatted(answer.getQuestion().getId());
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String vote (Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = answerService.getAnswer(id);
+        SiteUser siteUser = userService.getUser(principal.getName());
+
+        answerService.vote(answer, siteUser);
+
+        return "redirect:/question/detail/%s".formatted(answer.getQuestion().getId());
+    }
 }
